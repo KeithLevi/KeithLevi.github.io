39bf404bb0260f4c21bb7e3c3e86c1c28ac2a11d
Keith Levi <guilddrive@gmail.com>
2021-03-20 22:15:08 -0500

    updating the website skeleton

--- cs303/labW1D1/functionAndArrayTests.js
+++ cs303/labW1D1/functionAndArrayTests.js
@@ -1,5 +1,5 @@
 "use strict";
-/* global assert maxOfThree sum multiply findLongestWord reverseArray reverseArrayInPlace*/
+/* global assert maxOfThree sum multiply findLongestWord reverseArray reverseArrayInPlace scoreExams */
 
 /* 1.	1.	Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.  */
 describe("maxOfThree", function () {
@@ -47,15 +47,36 @@ Arrays have a reverse method that changes the array by inverting the order in wh
 */
 describe("reverseArray", function () {
     it("tests reverseArray odd number elements", function () {
-        assert.strictDeepEqual(reverseArray(["A", "B", "C"]), ["C", "B", "A"]);
+        assert.deepEqual(reverseArray(["A", "B", "C"]), ["C", "B", "A"]);
     });
     it("tests reverseArray even number elements", function () {
-        assert.strictDeepEqual( reverseArrayInPlace(["A", "B", "C", "D"]), ["D", "C", "B", "A"]);
+        assert.deepEqual(reverseArrayInPlace(["A", "B", "C", "D"]), ["D", "C", "B", "A"]);
     });
     it("tests reverseArrayInPlace even number elements", function () {
-        assert.strictDeepEqual(reverseArrayInPlace(["A", "B", "C", "D"]), ["D", "C", "B", "A"]);
+        assert.deepEqual(reverseArrayInPlace(["A", "B", "C", "D"]), ["D", "C", "B", "A"]);
     });
     it("tests reverseArrayInPlace odd number elements", function () {
-        assert.strictDeepEqual(reverseArray([1, 2, 3, 4, 5]), [5, 4, 3, 2, 1]);
+        assert.deepEqual(reverseArray([1, 2, 3, 4, 5]), [5, 4, 3, 2, 1]);
+    });
+});
+
+/*
+5.  Write a function, scoreExams, that takes an array of arrays of student answers and an array of the correct answers.  
+It should compare each student’s answers against the correct answers and return an array holding the scores of each student.  The score 
+for each student is a count of the number of correct answers (i.e., matches with the key of correct answers).  For example
+
+const studentAnswers = [[1, 1, 2], [2, 1, 2], [3, 1, 3]];
+const correctAnswers = [3, 1, 2];
+scoreExams(studentAnswers, correctAnswers)); --> [2, 2, 2]
+*/
+/*
+4.	Reverse an Array 
+Arrays have a reverse method that changes the array by inverting the order in which its elements appear. For this exercise, write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument by reversing its elements. Neither may use the standard reverse method. 
+*/
+describe("score exam", function () {
+    const studentAnswers = [[1, 1, 2], [2, 1, 2], [3, 1, 3]];
+    const correctAnswers = [3, 1, 2];
+    it("exam with 3 students", function () {
+        assert.deepEqual(scoreExams(studentAnswers, correctAnswers), [2, 2, 2]);
     });
 });
