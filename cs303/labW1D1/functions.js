e8196b9e4de279344b6ff5829d5d1e06f197e45b
Keith Levi <guilddrive@gmail.com>
2021-03-22 13:47:34 -0500

    ready for browser testing

--- cs303/labW1D1/functions.js
+++ cs303/labW1D1/functions.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */ 
-module.exports = {maxOfThree, sum, multiply };  //add all of your function names here that you need for the node 
+//module.exports = {maxOfThree, sum, multiply };  //add all of your function names here that you need for the node 
 //exports.maxOfThree = maxOfThree;
 
 
