3bf6df10a42310b2111c702fb25323e8f62f56c6
Keith Levi <guilddrive@gmail.com>
2021-03-26 23:23:19 -0500

    cleanup

--- cs303/labW1D6/arrayPractice.js
+++ cs303/labW1D6/arrayPractice.js
@@ -9,32 +9,29 @@ return arr.map(num => num * 2);
 }
 
 function doubleAges(arr){
-    return arr.map(person => {
-        const cloned = JSON.parse(JSON.stringify(person));
-        cloned.age = 2 * cloned.age;
-    return cloned;});
+
 }
 
 function filterEven(arr){
-    return arr.filter(num => num % 2 === 0);
+
 }
 
 function filterOver10(arr) {
-    return arr.filter(person => person.age > 10);
+
 }
 
 function findEvenNum(arr){
-    return arr.find(num => num % 2 === 0);
+
 }
 
 function findEvenAge(arr){
-  return arr.find(person => person.age % 2 === 0);
+
 }
 
 function includesEvenNum(arr){
-    return arr.includes(num => num % 2 === 0);
+ 
 }
 
 function includesEvenAge(arr){
-  return arr.includes(person => person.age % 2 === 0);
+
 }
--- cs303/labW1D6/destructure.js
+++ cs303/labW1D6/destructure.js
@@ -6,24 +6,10 @@
 
 function topSalary(salaries) {
 
-    if (Object.keys(salaries).length === 0) { return null;}
-    let max = 0;
-    let maxName = null;
-  
-    for(const [name, salary] of Object.entries(salaries)) {
-      if (max < salary) {
-        max = salary;
-        maxName = name;
-      }
-    }
-  
-    return maxName;
+
   }
 
 
   function getDateAgo(date, days) {
-    let dateCopy = new Date(date);
-  
-    dateCopy.setDate(date.getDate() - days);
-    return dateCopy.getDate();
+
   }
